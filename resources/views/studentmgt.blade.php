bf0bcba667ee8d67cf00468aecb8ecaa541094de
PSKoulouris <philippe.koulouris@gmail.com>
2025-09-11 17:21:47 +0200

    management system update delete

--- resources/views/studentmgt.blade.php
+++ resources/views/studentmgt.blade.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student management file</title>
 
-     @vite('resources/css/app.css')
+     @vite('resources/css/app.css') <!-- Link to Tailwind CSS -->
 
 </head>
+<!-- Form with submission to database -->
+
 <body class = "font-sans antialiased">
 <div class = "px-6 py-6 flex flex-col items-center justify-center bg-gray-50 text-black-50">
 
@@ -26,6 +28,67 @@
         <p class = "mt-2 text-green-600 font-semibold">{{ session("status") }}</p>
     @endif
 </div>  
+<!-- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////--
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+
+<!-- Table with update, create,. delete, cancel actions -->
+
+<table>
+    <tr>
+        <td>ID:</td>
+        <td>Email:</td>
+        <td>Created at:</td>
+        <td>Updated at:</td>
+        <td colspan = "2" >Actions:</td>
+    </tr>
+
+    @foreach ( $listStudent as $itemStudent )
+
+        <tr>    
+            <form method = "POST" action ="{{ route("updateStudent", $itemStudent)}}" accept-charset="UTF-8" ">
+            {{ csrf_field() }}
+                <td>
+                    {{ $itemStudent-> id }}
+                </td>
+                <td>
+                    <input type = "email" name="name_email" value = {{ $itemStudent->email }}>
+                </td>
+                <td>
+                    <input type = "password" name = "name_password" value = {{ $itemStudent->password }}>
+                </td>
+                <td>
+                    {{ $itemstudent->created_at }}
+                </td>
+                <td>
+                    {{ $itemStudent->updated_at }}
+                </td>
+                <td>
+                    <input type = "submit" value = "Update">
+                </td>
+            </form>
+
+                <td>
+                    <form action = "POST" method="{{ route("deleteStudent", $itemStudent->id) }}">
+                    {{ csrf_field() }}
+                        <input type = "submit" value = "Delete">
+                    </form>
+                </td>
+        </tr>
+        @endforeach 
+</table>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 </body>
