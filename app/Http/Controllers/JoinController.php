28abf15d0b209bae17b50620732ee0cecd1008d8
PSKoulouris <philippe.koulouris@gmail.com>
2025-09-14 13:15:04 +0200

    management system one to many

--- app/Http/Controllers/JoinController.php
+++ app/Http/Controllers/JoinController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+
 
 class JoinController extends Controller
 {
-    //
+    public function saveJoin (Request $request) {
+        Log::info(json_encode($request->all()))
+
+        try {
+            //Begin transaction:
+            DB::beginTransaction();
+
+            //Save data to databse:
+            DB::commit();
+            //Stay on page:
+            return redirect("/savemgt");
+
+        } catch (Exception $e){
+
+            //Cancel transaction:
+            DB::rollBack();
+            //Log error
+            Log::error("Transaction rolledback due to an error: " . $e->getMessage());
+            //redirect to page error message:
+            return back()->withErrors("An error occured during transaction saveJoin");
+
+        }
+    }
 }
